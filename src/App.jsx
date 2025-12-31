db6791e05cb1e421ea0ac8bc381e2373177f8cfa
Somaya Ataie <somayaatee0@gmail.com>
2025-12-31 21:54:43 +0430

    create

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+function App() {
+  return (
+    <div className="page">
+      <header className="header">
+        <div>
+        <h1 className="title">Expense Tracker</h1>
+        <p className="subtitle">week1 + week2 pracrice project</p>
+        </div>
+      </header>
+        <section className="card">
+          <div className="cardHeader">
+            <h2 className="sectionTitle">Add Expense</h2>
+          </div>
+        <div className="cardBody">
+          <p>Form will be here....</p>
+        </div>
+
+        </section>
+      
+      
+    </div>
+  );
+}
+
+export default App;
