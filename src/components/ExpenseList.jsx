234163142bf0d64ab1e01e875289c2c5ac142c41
Somaya Ataie <somayaatee0@gmail.com>
2026-01-02 11:08:36 +0430

    cretae component

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,10 +1,14 @@
 import Expenses from "./ExpenseItem";
 
-export default function ExpenseList({ expenses }) {
+export default function ExpenseList({ expenses,onDeleteExpense }) {
+  if(expenses.lenght == 0){
+    <p className="empty">No expenses yet. Add your first expenses ✅</p>
+  }
+
   return (
     <ul className="list">
       {expenses.map((expense) => (
-        <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
+        <ExpenseItem key={expense.id} expense={expense}  onDelete={onDeleteExpense}></ExpenseItem>
       ))}
     </ul>
   );
