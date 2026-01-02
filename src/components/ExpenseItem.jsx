af5c9ee853ee9c786734ad1def2bd94292569883
Somaya Ataie <somayaatee0@gmail.com>
2026-01-02 11:12:55 +0430

    add button in component item

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,12 @@
-export default function ExpenseItem({expenses}){
+export default function ExpenseItem({expenses,onDelete}){
     return(
         <li className="item">
             <div>
-                <div className="itemTitle">
-                    {expenses.title}
-                </div>
-                <div className="itemMeta">
-                    {expenses.category}
-                </div>
+                <div className="itemTitle">  {expenses.title} </div>
+                <div className="itemMeta"> {expenses.category} </div>
             </div>
-            <div className="amount">
-                ${expenses.amount}
-            </div>
+            <div className="amount"> ${expenses.amount}</div>
+            <button className="btn sm danger" onClick={()=> onDelete(expenses.id)}>Delete</button>
         </li>
     )
 }
